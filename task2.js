9fb263010237e60d1a2f71b0c36bc2dbb12f0667
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2026-01-14 23:15:47 -0600

    Creando funtion listUsers con fetch

--- task2.js
+++ task2.js
@@ -1,2 +1,6 @@
 // Task 2: listUsers()
-
+export function listUsers() {
+    fetch('http://localhost:3000/users')
+        .then(respuesta => respuesta.json())
+        .then(datos => console.log(datos))
+}
