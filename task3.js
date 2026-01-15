2f6ce9f559e0f0ec0d0e6de66483ed2f72320b16
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2026-01-14 23:49:26 -0600

    Creando funtion addUser con incremento de ID

--- task3.js
+++ task3.js
@@ -1,2 +1,28 @@
 // Task 3: addUser(first_name, last_name, email)
+export async function addUser(first_name, last_name, email) {
 
+    let idUser;
+
+    
+    const response = await fetch('http://localhost:3000/users');
+    const users = await response.json();
+
+    idUser = users.length + 1;
+    console.log('ID generado:', idUser);
+
+  const postResponse = await fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      id: idUser,
+      first_name,
+      last_name,
+      email
+    }),
+  });
+
+  const data = await postResponse.json();
+  console.log('Usuario creado:', data);
+}
